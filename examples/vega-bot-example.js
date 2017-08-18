3167173aff469f6bef1fd960eb0e573422142a06
nwhitmont <v-niwh@microsoft.com>
2017-08-17 17:49:31 -0700

    add examples

--- examples/vega-bot-example.js
+++ examples/vega-bot-example.js
@@ -1,40 +1,29 @@
-# botbuilder-vega
-
-Bot Framework Node.js plugin to enable sending Vega charts rendered to PNG in your bot's messages.
-
-### Install
-1.  `npm install botbuilder-vega --save`
-
-### Usage
-1. In your bot code, import the module: `var sendVega = require('botbuilder-vega');`
-2. In your bot dialog, use `sendVega(session, "Message text", vegaSpec)` instead of `session.send()`.
-
-### Example Bot
-```js
 var builder = require('botbuilder');
 var restify = require('restify');
-var sendVega = require('../lib/botbuilder-vega');
+var sendVega = require('../lib/botbuilder-vega').sendVega;
 
 var countyUnemploymentMap = require('./charts/county-unemployment.vg.json');
 
-// Server setup
+//Server setup
 var server = restify.createServer();
+
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
-// Get secrets from server environment
+//Get secrets from server environment
 var connector = new builder.ChatConnector({appId: process.env.MICROSOFT_APP_ID, appPassword: process.env.MICROSOFT_APP_PASSWORD});
 
-// Create chat bot
+//Create chat bot
 var bot = new builder.UniversalBot(connector);
 
-// Handle bot framework messages
+//Handle bot framework messages
 server.post('/api/messages', connector.listen());
 
-// root dialog
+/* Dialogs */
 bot.dialog('/', function (session) {
     session.send('Sending chart example:');
     sendVega(session, "US County Unemployment Map", countyUnemploymentMap); 
 });
-```
+
+// END OF LINE
